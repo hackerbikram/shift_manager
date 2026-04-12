f1882c10939c66d9f376e3f0979e6f59429b7d83
hackerbikram <166804821+hackerbikram@users.noreply.github.com>
2026-04-12 16:57:25 +0900

    feat: initial commit

--- src/app/(pages)/shift/page.tsx
+++ src/app/(pages)/shift/page.tsx
@@ -1,12 +1,19 @@
 'use client'
-import Schedulercreater from "@/components/shift_components/shift_create"
-import ShiftCalendar from "@/components/shift_components/shift_calander"
+import Schedulercreater from "@/components/shift_components/shift_create";
+import  ShiftCalendar  from '@/components/shift_components/shift_calander';
 export default function ShiftPage(){
   return (
-    <div>
-      <Schedulercreater />
+    <div className="p-5 justify-center flex gap-5 m-5">
       <div>
-        <ShiftCalendar />
+        <button
+        className="bg-green-400 rounded-xl p-4 text-black"
+        >
+          Create shift 
+          </button>
+          <button
+          className="bg-green-400 rounded-xl p-4"
+          > add new subject
+          </button>
       </div>
     </div>
   )
